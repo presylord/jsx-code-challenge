7a022ce6738b5a320dc6692d3de8f0131c3cd80b
presylord <91861180+presylord@users.noreply.github.com>
2021-11-30 01:44:22 +0800

    minor update

--- src/index.js
+++ src/index.js
@@ -11,11 +11,20 @@ ReactDOM.render(
     <h1 className="header" contentEditable="true" spellCheck="false">
       Operating Systems for Desktops:
     </h1>
-    <ul>
-      <li>Windows</li>
-      <li>MacOS</li>
-      <li>Linux</li>
-    </ul>
+    <div>
+      <img
+        class="os"
+        src="https://cdn.vox-cdn.com/uploads/chorus_image/image/66098561/mswindows2_2040.0.0.jpg"
+      />
+      <img
+        class="os"
+        src="https://www.techrepublic.com/a/hub/i/r/2017/12/11/9fac1c13-984f-429c-83a8-1a7a286271ae/resize/1200x/3a6056002ad89cefbd930cd12fb78e0b/macoshero.jpg"
+      />
+      <img
+        class="os"
+        src="https://bloximages.newyork1.vip.townnews.com/redandblack.com/content/tncms/assets/v3/editorial/4/59/45940eb2-5403-11e9-a843-db0e4491cc90/5ca13d8453042.image.jpg"
+      />
+    </div>
   </div>,
   document.getElementById("root")
 );
